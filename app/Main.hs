228bade15f520b7f805670374ee09031028f0568
cleissonbarbosa <cleisson147@gmail.com>
2025-02-02 23:25:28 -0300

    Feat: Enhance network scanner functionality with concurrent port scanning and service detection; update dependencies and .gitignore

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,23 @@ module Main (main) where
 import Network.Socket
 import qualified Data.ByteString.Char8 as B
 import Control.Exception (try, IOException)
+import Control.Concurrent.Async (mapConcurrently)
+import System.IO (writeFile)
+import Data.Time (getCurrentTime)
 
--- Função para escanear uma porta específica
-scanPort :: HostName -> PortNumber -> IO ()
+-- Escanear uma porta específica e retornar o resultado como String
+scanPort :: HostName -> PortNumber -> IO String
 scanPort host port = do
     addr <- resolve host port
     sock <- openSocket' addr
     result <- tryConnect sock addr
-    case result of
-        Just _  -> putStrLn $ "Port " ++ show port ++ " is open"
-        Nothing -> putStrLn $ "No connection to port " ++ show port
+    res <- case result of
+        Just _  -> do
+            let service = detectService port
+            return $ "Port " ++ show port ++ " is open (" ++ service ++ ")"
+        Nothing -> return $ "No connection to port " ++ show port
     close sock
+    return res
 
 -- Resolver o endereço do host
 resolve :: HostName -> PortNumber -> IO AddrInfo
@@ -34,8 +40,29 @@ tryConnect sock addr = do
     result <- try (connect sock (addrAddress addr)) :: IO (Either IOException ())
     return $ either (const Nothing) Just result
 
+-- Detectar serviço com base em portas conhecidas
+detectService :: PortNumber -> String
+detectService 80    = "HTTP"
+detectService 443   = "HTTPS"
+detectService 22    = "SSH"
+detectService 21    = "FTP"
+detectService 8080  = "HTTP-Alt"
+detectService _     = "Unknown Service"
+
+-- Função principal
 main :: IO ()
 main = do
     let host = "127.0.0.1"
-    let ports = [80, 443, 8080]
-    mapM_ (scanPort host) ports
+    -- Porta específicas e intervalos de portas
+    let singlePorts = [80, 443, 8080]
+    let rangePorts = [75..85]
+    let ports = singlePorts ++ rangePorts
+    -- Escaneia de forma concorrente
+    results <- mapConcurrently (scanPort host) ports
+    -- Imprime os resultados no console
+    mapM_ putStrLn results
+    -- Obter data e hora atuais
+    time <- getCurrentTime
+    let reportHeader = "Scan report generated at: " ++ show time ++ "\n\n"
+    -- Gera relatório em arquivo com data e hora
+    writeFile "scan_report.txt" (reportHeader ++ unlines results)
